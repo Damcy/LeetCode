cc2e8b4bd43bdfe862d04e18c3814cfa44b72e73
Damcy <stmayue@gmail.com>
2015-12-18 15:19:23 +0800

    add solution

---
+++ Group_Anagrams.cpp
@@ -0,0 +1,43 @@
+// author: Ien
+// create on: 2015-12-17
+// description: LeetCode Group Anagrams
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        vector<vector<string> > groupAnagrams(vector<string>& strs) {
+            // construct a map to classify strs
+            // key is sorted str
+            // value are elements in strs
+            map<string, vector<string> > hmap;
+            for (int i = 0; i < strs.size(); ++i) {
+                string key = strs[i];
+                // fine the key
+                sort(key.begin(), key.end());
+                map<string, vector<string> >::iterator it = hmap.find(key);
+                // then insert it to the map according to its key
+                if (it != hmap.end()) {
+                    it->second.push_back(strs[i]);
+                } else {
+                    vector<string> temp;
+                    temp.push_back(strs[i]);
+                    hmap.insert(pair<string, vector<string> >(key, temp));
+                }
+            }
+
+            // construct result
+            vector<vector<string> > res;
+            for (map<string, vector<string> >::iterator it = hmap.begin();
+                    it != hmap.end(); ++it) {
+                // sort values in the same key
+                sort(it->second.begin(), it->second.end());
+                res.push_back(it->second);
+            }
+
+            return res;
+        }
+};
---
+++ Pow_x_n.cpp
@@ -0,0 +1,33 @@
+// author: Ien
+// create on: 2015-12-18
+// description: LeetCode Pow(x, n)
+
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+    public:
+        double myPow(double x, int n) {
+            if (n == 0) {
+                return 1;
+            } else if (n == 1) {
+                return x;
+            }
+            double half = myPow(x, abs(n) / 2);
+            if (n > 0) {
+                if(n & 1) {
+                    return half * half * x;
+                } else {
+                    return half * half;
+                }
+            } else {
+                n = abs(n);
+                if(n & 1) {
+                    return 1.0 / (half * half * x);
+                } else { 
+                    return 1.0 / (half * half);
+                }
+            }
+        }
+};
