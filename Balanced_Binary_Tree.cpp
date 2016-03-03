d6d81230bb63b0cee840f7c6fa94f442acbf6eda
Damcy <stmayue@gmail.com>
2016-03-03 11:31:07 +0800

    add solution

--- Balanced_Binary_Tree.cpp
+++ Balanced_Binary_Tree.cpp
@@ -9,17 +9,20 @@
  */
 class Solution {
 public:
+	// 遍历一次完成平衡检查
     bool check_balance(TreeNode* root, int* depth) {
     	if (!root) {
     		*depth = 0;
     		return true;
     	}
 
+    	// 在判断子树平衡的同时记忆子树的深度
     	int left_dep = 0;
     	int right_dep = 0;
     	bool left_balance = check_balance(root->left, &left_dep);
     	bool right_balance = check_balance(root->right, &right_dep);
 
+    	// 子树平衡的同时 更新当前的root的深度
     	if (left_balance && right_balance) {
     		int dep_diff = abs(left_dep - right_dep);
     		if (dep_diff <= 1) {
